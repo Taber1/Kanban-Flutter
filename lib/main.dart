c501aef404fc6d02fde00c1d41f9e164b7f4f60e
Taber Bin Zameer <tbzameer@gmail.com>
2023-02-25 22:38:30 +0500

    Basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,333 @@
 import 'package:flutter/material.dart';
+import 'package:csv/csv.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Kanban Board',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: KanbanBoard(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class KanbanBoard extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _KanbanBoardState createState() => _KanbanBoardState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _KanbanBoardState extends State<KanbanBoard> {
+  List<List<dynamic>> csvData = [
+    ['Task Name', 'Description', 'Time Spent', 'Date Completed']
+  ];
 
-  void _incrementCounter() {
+  List<Task> toDoTasks = [
+    Task(name: 'Task 1', description: 'Description of Task 1'),
+    Task(name: 'Task 2', description: 'Description of Task 2'),
+    Task(name: 'Task 3', description: 'Description of Task 3'),
+  ];
+
+  List<Task> inProgressTasks = [];
+
+  List<Task> doneTasks = [];
+
+  final TextEditingController _taskNameController = TextEditingController();
+
+  final TextEditingController _taskDescriptionController =
+      TextEditingController();
+
+  void _addTask(String taskName, String taskDescription) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      toDoTasks.add(Task(name: 'taskName', description: taskDescription));
     });
+    Navigator.pop(context);
+  }
+
+  void _stopTask(Task task) {
+    setState(() {
+      task.isRunning = false;
+      final timeSpent =
+          DateTime.now().difference(task.startTime!).inSeconds / 60.0;
+      task.timeSpent += timeSpent as Duration;
+      _addToCsv(task.name, task.description, task.timeSpent as double,
+          DateTime.now());
+    });
+  }
+
+  void _moveTask(Task task, List<Task> targetList) {
+    setState(() {
+      if (toDoTasks.contains(task)) {
+        toDoTasks.remove(task);
+      } else if (inProgressTasks.contains(task)) {
+        inProgressTasks.remove(task);
+      } else if (doneTasks.contains(task)) {
+        doneTasks.remove(task);
+      }
+      targetList.add(task);
+    });
+  }
+
+  void _showNewTaskDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('New Task'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(
+                controller: _taskNameController,
+                decoration: InputDecoration(
+                  hintText: 'Task Name',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                controller: _taskDescriptionController,
+                decoration: InputDecoration(
+                  hintText: 'Task Description',
+                ),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Add'),
+              onPressed: () {
+                _addTask(
+                    _taskNameController.text, _taskDescriptionController.text);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _addToCsv(String name, String description, double timeSpent,
+      DateTime dateCompleted) async {
+    List<dynamic> newRow = [name, description, timeSpent, dateCompleted];
+    csvData.add(newRow);
+    final dir = await getApplicationDocumentsDirectory();
+    final path = dir.path + '/tasks.csv';
+    final file = File(path);
+    String csv = const ListToCsvConverter().convert(csvData);
+    file.writeAsString(csv);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Kanban Board'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.file_download),
+            onPressed: () async {
+              final dir = await getApplicationDocumentsDirectory();
+              final path = dir.path + '/tasks.csv';
+              final file = File(path);
+              String csv = const ListToCsvConverter().convert(csvData);
+              file.writeAsString(csv);
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('CSV file exported to ${dir.path}/tasks.csv'),
+                ),
+              );
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _buildKanbanColumn('To Do', toDoTasks),
+                _buildKanbanColumn('In Progress', inProgressTasks),
+                _buildKanbanColumn('Done', doneTasks),
+              ],
             ),
+          ),
+          Container(
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                ElevatedButton(
+                  child: Text('New Task'),
+                  onPressed: _showNewTaskDialog,
+                ),
+                ElevatedButton(
+                  child: Text('Export CSV'),
+                  onPressed: () async {
+                    final dir = await getApplicationDocumentsDirectory();
+                    final path = dir.path + '/tasks.csv';
+                    final file = File(path);
+                    String csv = const ListToCsvConverter().convert(csvData);
+                    file.writeAsString(csv);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content:
+                            Text('CSV file exported to ${dir.path}/tasks.csv'),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildKanbanColumn(String title, List<Task> tasks) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              title,
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16.0),
+            Expanded(
+              child: ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final task = tasks[index];
+                  return Card(
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          title: Text(task.name),
+                          subtitle: Text(task.description),
+                          trailing: IconButton(
+                            icon: task.isRunning
+                                ? Icon(Icons.pause)
+                                : Icon(Icons.play_arrow),
+                            onPressed: () {
+                              if (task.isRunning) {
+                                _stopTask(task);
+                              } else {
+                                _startTask(task);
+                              }
+                            },
+                          ),
+                        ),
+                        ButtonBar(
+                          children: <Widget>[
+                            TextButton(
+                              child: const Text('Delete'),
+                              onPressed: () {
+                                setState(() {
+                                  tasks.remove(task);
+                                });
+                              },
+                            ),
+                            TextButton(
+                              child: const Text('Edit'),
+                              onPressed: () {},
+                            ),
+                            TextButton(
+                              child: const Text('Move'),
+                              onPressed: () {
+                                _showMoveTaskDialog(task);
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  List<KanbanColumn> kanbanColumns = [];
+  void _showMoveTaskDialog(Task task) {
+    final currentIndex =
+        kanbanColumns.indexWhere((element) => element.tasks.contains(task));
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Move Task'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: kanbanColumns
+                  .map(
+                    (column) => RadioListTile(
+                      title: Text(column.title),
+                      value: column,
+                      groupValue: kanbanColumns[currentIndex],
+                      onChanged: (value) {
+                        setState(() {
+                          kanbanColumns[currentIndex].tasks.remove(task);
+                          value!.tasks.add(task);
+                        });
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _startTask(Task task) {
+    setState(() {
+      task.isRunning = true;
+      task.startTime = DateTime.now();
+    });
+  }
+}
+
+class KanbanColumn {
+  final String title;
+  final List<Task> tasks;
+
+  KanbanColumn({required this.title, required this.tasks});
+}
+
+class Task {
+  final String name;
+  final String description;
+  Duration timeSpent;
+  bool isRunning;
+  DateTime? startTime;
+
+  Task({
+    required this.name,
+    this.description = '',
+    this.timeSpent = const Duration(seconds: 0),
+    this.isRunning = false,
+    this.startTime,
+  });
 }
